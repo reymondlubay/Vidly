9f1e006cebac765fc76605c4ab96b4ea6333df32
Reymond Lubay <reymond.lubay@gmail.com>
2019-08-05 22:41:58 +0800

    Create Customer CRUD API

--- Controllers/Api/CustomersController.cs
+++ Controllers/Api/CustomersController.cs
@@ -10,6 +10,71 @@ namespace Vidly.Controllers.Api
 {
     public class CustomersController : ApiController
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // Get /Api/Customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        // Get /Api/Customers/1
+        public Customer GetCustomers(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            return customer;
+        }
+
+        // POST /Api/Customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        // PUT /Api/Customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthdate = customer.Birthdate;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+
+        }
+
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            _context.Customers.Remove(customerInDb);
+            _context.SaveChanges();
+        }
 
     }
 }
